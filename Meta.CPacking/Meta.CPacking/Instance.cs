dfffcc45b8a37fab64a04cc6a821bc7ed850e58f
Ahmad Hashemi <ahmadh@campus.uni-paderborn.de>
2017-06-12 22:15:17 +0200

    1. Commit - daten einlesen

---
+++ Meta.CPacking/Meta.CPacking/Instance.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.IO;
+
+namespace Meta.CPacking
+{
+    class Instance
+    {
+        public int NumContainers;
+        public int[,] Containers;
+        public int NumBoxes;
+        public int[,] Boxes;
+
+        public Instance (string path)
+        {
+            Initialization(path);
+        }
+
+        private void Initialization(string InPath)
+        {
+            var Temp = File.ReadLines(InPath)
+                .Select(line => line.Split(' '))
+                .ToArray();
+
+            NumContainers = int.Parse(Temp[0][1]);
+
+            Containers = new int[NumContainers, 4];
+            for (int i = 1; i <= NumContainers; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Containers[i - 1, j] = int.Parse(Temp[i][j]);
+                }
+            }
+
+            NumBoxes = int.Parse(Temp[NumContainers + 1][1]);
+
+            Boxes = new int[NumBoxes, 5];
+            for (int i = NumContainers + 2; i < NumContainers + NumBoxes + 2; i++)
+                for (int j = 0; j < 5; j++)
+                    Boxes[i - NumContainers - 2, j] = int.Parse(Temp[i][j]);
+        }
+    }
+}
---
+++ Meta.CPacking/Meta.CPacking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Meta.CPacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"F:\Z Studiumsablauf Z\6. Semester\Metaheuristik\Projekt\Instances\instance_12_1000_4.txt";
+
+            Instance expl = new Instance(Path);
+
+            Console.WriteLine(" number of containers is: "+ expl.NumContainers);
+            Console.WriteLine(" nuber of Boxes is: " + expl.NumBoxes);
+            Console.ReadKey();
+        }
+    }
+}
